982f65b7115b90414490990b924a50259448693c
emerrafter1 <emerlouiserafter@gmail.com>
2025-05-02 15:25:38 +0100

    project set up

---
+++ data.py
@@ -0,0 +1,17 @@
+question_data = [
+    {"text": "A slug's blood is green.", "answer": "True"},
+    {"text": "The Great Wall of China is visible from space.", "answer": "False"},
+    {"text": "Lightning never strikes the same place twice.", "answer": "False"},
+    {"text": "Humans and dinosaurs coexisted.", "answer": "False"},
+    {"text": "Goldfish have a memory span of three seconds.", "answer": "False"},
+    {"text": "The human body has four lungs.", "answer": "False"},
+    {"text": "Bananas grow on trees.", "answer": "False"},
+    {"text": "There are five different blood groups.", "answer": "False"},
+    {"text": "Adults have fewer bones than babies.", "answer": "True"},
+    {"text": "The capital of Australia is Sydney.", "answer": "False"},
+    {"text": "Sharks are mammals.", "answer": "False"},
+    {"text": "The human nose can detect over one trillion smells.", "answer": "True"},
+    {"text": "Octopuses have three hearts.", "answer": "True"},
+    {"text": "The speed of light is about 300,000 kilometers per second.", "answer": "True"},
+    {"text": "Venus is the hottest planet in our solar system.", "answer": "True"}
+]
